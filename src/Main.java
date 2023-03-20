a6a0ed17aad3a8b2c765ed3b8269562f949fa894
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-20 19:25:54 +0300

    add houses List

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    List<House> houses = new ArrayList<>();
+
+    House house1 = new Private_House( "Земля");
+    houses.add(house1);
+
+    House house2 = new Private_House( "Луна");
+    houses.add(house2);
+
+    House house3 = new ApartmentHouse("Марс");
+    houses.add(house3);
+
+    House house4 = new ApartmentHouse("Меркурий");
+    houses.add(house4);
+
   }
 }
