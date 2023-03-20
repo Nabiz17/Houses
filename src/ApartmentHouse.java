fa4f79bcc21d4b946729cc1ab0d94a67bb13cd02
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-20 19:03:46 +0300

    add class ApartmentHouse

---
+++ src/ApartmentHouse.java
@@ -0,0 +1,13 @@
+public class ApartmentHouse extends House{
+
+  private int numberOfTrips;
+  //Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
+  //При создании многоквартирного дома дополнительно принимайте
+  // в конструкторе целое число - количество подъездов.
+  public ApartmentHouse(String address) {
+    super(address);
+    this.numberOfTrips = numberOfTrips;
+  }
+
+
+}
