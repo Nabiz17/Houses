06adc798b1d9d9c33d7cec0771ed36db35e4f213
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-20 18:50:31 +0300

    add class Private_HouseHome

---
+++ src/Private_House.java
@@ -0,0 +1,8 @@
+public class Private_House extends House {
+
+  //Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
+
+  public Private_House(String address) {
+    super(address);
+  }
+}
